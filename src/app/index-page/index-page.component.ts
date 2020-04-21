cb3927d34d048407e1be970d64ce279d6d4450a6
shamantvg <63244826+shamantvg@users.noreply.github.com>
2020-04-21 19:56:30 +0530

    sweetalert added

--- src/app/index-page/index-page.component.ts
+++ src/app/index-page/index-page.component.ts
@@ -3,7 +3,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import * as $ from "jquery";
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { FieldsService } from '../fields.service';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -11,7 +11,7 @@ import { ExcelService } from '../excel.service';
 //import { DataTablesModule } from 'angular-datatables';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { AngularMaterialModule } from '../angular-material.module';
-
+import swal from 'sweetalert';
 
 
 
@@ -30,9 +30,10 @@ export class IndexPageComponent implements OnInit {
 
   }
 
-  displayedColumns: string[] = ['id', 'employee_name', 'employee_salary', 'employee_age', 'profile_image'];
-  dataSource : MatTableDataSource<any>;
-  
+
+  displayedColumns: string[] = ['serial_number', 'type', 'OEM', 'model', 'procure_date', 'warranty_start_date', 'warranty_end_date', 'amc_startdate', 'amc_endate', 'os_image', 'is_customer_compliant', 'customer_dept', 'id'];
+  dataSource: MatTableDataSource<any>;
+
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -47,6 +48,21 @@ export class IndexPageComponent implements OnInit {
   editAssetForm_tag = null;
   assignAssetForm_tag = null;
   selectedRowIndex: number = -1;
+  exportData = [];
+  employeeList = [];
+  SearilaNUm_selected = "";
+  current_assiged_emp = null;
+  customer_dept = "";
+  type_sel = "";
+  OEM = "";
+  model = "";
+  procure_date ="";
+  warranty_start_date = "";
+  warranty_end_date = "";
+  amc_startdate = "";
+  amc_endate = "";
+  os_image = "";
+  is_customer_compliant = "";
 
   title = '';
 
@@ -54,8 +70,8 @@ export class IndexPageComponent implements OnInit {
 
   openModal(template: TemplateRef<any>) {
 
-    if((this.selectedRowIndex < 1) || (this.selectedRowIndex === undefined)){
-      alert("please select a asset !!!");
+    if ((this.selectedRowIndex < 1) || (this.selectedRowIndex === undefined)) {
+      swal("Information", "Please select a asset !!!", "info");
       return false;
     }
 
@@ -76,8 +92,8 @@ export class IndexPageComponent implements OnInit {
 
   open_editAssetDiv() {
 
-    if((this.selectedRowIndex < 1) || (this.selectedRowIndex === undefined)){
-      alert("please select a asset !!!");
+    if ((this.selectedRowIndex < 1) || (this.selectedRowIndex === undefined)) {
+      swal("Information", "Please select a asset !!!", "info");
       return false;
     }
 
@@ -89,13 +105,14 @@ export class IndexPageComponent implements OnInit {
 
   open_assignAssetDiv() {
 
-    
 
-    if((this.selectedRowIndex < 1) || (this.selectedRowIndex === undefined)){
-      alert("please select a asset !!!");
+
+    if ((this.selectedRowIndex < 1) || (this.selectedRowIndex === undefined)) {
+      swal("Information", "Please select a asset !!!", "info");
       return false;
     }
 
+    this.GetUniqEmployee();
     this.grid_tag = null;
     this.editAssetForm_tag = null;
     this.assignAssetForm_tag = true;
@@ -120,7 +137,19 @@ export class IndexPageComponent implements OnInit {
 
   rowClicked(rowval) {
     this.selectedRowIndex = rowval.id;
-    console.log("rorval--->"+JSON.stringify(rowval));
+    this.SearilaNUm_selected = rowval.serial_number;
+    this.customer_dept = rowval.customer_dept;
+    this.type_sel = rowval.type;
+    this.OEM = rowval.OEM;
+    this.model = rowval.model;
+    this.procure_date = rowval.procure_date;
+    this.warranty_start_date = rowval.warranty_start_date;
+    this.warranty_end_date = rowval.warranty_end_date;
+    this.amc_startdate = rowval.amc_startdate;
+    this.amc_endate = rowval.amc_endate;
+    this.os_image = rowval.os_image;
+    this.is_customer_compliant = rowval.is_customer_compliant;
+    console.log("rorval--->" + JSON.stringify(rowval));
   }
   eleClicked(eleval) {
     //console.log("element val--->"+JSON.stringify(eleval));
@@ -130,6 +159,7 @@ export class IndexPageComponent implements OnInit {
     const userDetails = JSON.parse(localStorage.getItem('admiinDetails'));
     if (userDetails) {
       this.GetGrid();
+      this.GetEmployeeList();
       return true;
     } else {
       this.router.navigateByUrl('/login');
@@ -139,16 +169,16 @@ export class IndexPageComponent implements OnInit {
 
   GetGrid(): any {
 
-    console.log("ngOnInit");
-    this.FieldsList.getEmps().subscribe((result) => {
+    //console.log("ngOnInit");
+    this.FieldsList.getAllAsset().subscribe((result) => {
       //console.log("result--->" + JSON.stringify(result));
-      this.dataSource = new MatTableDataSource(result.data);
-
+      this.dataSource = new MatTableDataSource(result);
+      this.exportData = result;
       this.grid_tag = true;
       // this.dataSource.paginator = this.paginator;
       // this.dataSource.sort = this.sort;
-      
-      
+
+
       //   setTimeout(() => this.dataSource.paginator = this.paginator);
       // setTimeout(() => this.dataSource.sort = this.sort);
     });
@@ -156,28 +186,89 @@ export class IndexPageComponent implements OnInit {
     //let result = this.FieldsList.getEmps();
     //console.log("result--->" + JSON.stringify(result));
     //this.dataSource = new MatTableDataSource(result);
-    
-    
 
+
+
+  }
+
+  GetEmployeeList(): any {
+
+    //console.log("GetEmployeeList");
+    this.FieldsList.GetEmployeeList().subscribe((result) => {
+      this.employeeList = result;
+    });
+  }
+
+  GetUniqEmployee(): any {
+    this.FieldsList.GetUniqEmployee(this.selectedRowIndex).subscribe((result) => {
+      if (result[0].firstname) {
+        this.current_assiged_emp = result[0].firstname + " " + result[0].lastname;
+      } else {
+
+        this.current_assiged_emp = "IT Dept";
+      }
+
+    });
   }
 
 
 
 
-  AddAsset(form: NgForm) {
-
+  AddAsset(Addform: NgForm) {
+    //console.log(Addform.value);
+    this.FieldsList.addAssetFunc(Addform.value).subscribe((result) => {
+      //console.log("assign success");
+      swal("Success", "New Asset Details added successfully!!!", "success");
+      this.GetGrid();
+      this.reload_home();
+    }, err => {
+      swal("Error", "Error while adding asset !!!", "error");
+      //console.log(err);
+    });
   }
 
-  EditAsset(form: NgForm) {
-
+  EditAsset(Editform: NgForm) {
+    console.log(Editform.value);
+    this.FieldsList.editAssetFunc(Editform.value).subscribe((result) => {
+      //console.log("assign success");
+      swal("Success", "Asset Details successfully modified!!!", "success");
+      this.GetGrid();
+      this.reload_home();
+    }, err => {
+      swal("Error", "Error while updating asset !!!", "error");
+      //console.log(err);
+    });
   }
 
-  RemoveAsset(form: NgForm) {
+  RemoveAsset(Removeform: NgForm) {
+    //const params = new HttpParams().set('id', "'" + this.selectedRowIndex + "'");
 
+    // console.log(Removeform.value);
+    // return false;
+
+    this.FieldsList.RemoveAssetFunc(this.selectedRowIndex).subscribe((result) => {
+      //console.log("assign success");
+      this.GetGrid();
+      this.reload_home();
+      this.modalRef.hide();
+      swal("Success", "Asset deleted successfully!!!", "success");
+    }, err => {
+      swal("Error", "Please try later.!!!", "error");
+      //console.log(err);
+    });
   }
 
-  AssignAsset(form: NgForm) {
-
+  AssignAsset(Assignform: NgForm) {
+    //console.log(Assignform.value);
+    this.FieldsList.AssignReassignFunct(Assignform.value).subscribe((result) => {
+      //console.log("assign success");
+      swal("Success", "Asset Assigned successfully!!!", "success");
+      this.GetGrid();
+      this.reload_home();
+    }, err => {
+      swal("Error", "Please try later.!!!", "error");
+      //console.log(err);
+    });
   }
 
 
@@ -186,7 +277,6 @@ export class IndexPageComponent implements OnInit {
     this.router.navigateByUrl('/login');
   }
   reload_home() {
-    //alert("here");
     this.selectedRowIndex = -1;
     this.grid_tag = true;
     this.addAssetForm_tag = null;
@@ -212,38 +302,6 @@ export class IndexPageComponent implements OnInit {
     this.reload_home();
   }
 
-  exportData: any = [{
-    sl_number: '1',
-    Asset_Type: 'Desktop',
-    Serial_number: 'TM001',
-    Manufacter: 'HP',
-    Assigned: 'Ravi'
-  }, {
-    sl_number: '2',
-    Asset_Type: 'Laptop',
-    Serial_number: 'TM002',
-    Manufacter: 'DELL',
-    Assigned: 'Rakesh'
-  }, {
-    sl_number: '3',
-    Asset_Type: 'Desktop',
-    Serial_number: 'TM003',
-    Manufacter: 'HP',
-    Assigned: 'Vinod'
-  }, {
-    sl_number: '4',
-    Asset_Type: 'Laptop',
-    Serial_number: 'TM004',
-    Manufacter: 'DELL',
-    Assigned: 'Rohit'
-  }, {
-    sl_number: '5',
-    Asset_Type: 'Keyboard',
-    Serial_number: 'TM005',
-    Manufacter: 'Lenovo',
-    Assigned: 'Manoj'
-  }];
-
   assetType = [{
     Tname: 'Desktop',
   },
@@ -260,9 +318,11 @@ export class IndexPageComponent implements OnInit {
 
   AMATCompliant = [{
     desc: 'Yes',
+    id:'Y'
   },
   {
     desc: 'No',
+    id:'N'
   }
   ];
 
@@ -274,27 +334,7 @@ export class IndexPageComponent implements OnInit {
   }
   ];
 
-  employeeList = [{
-    id: 1,
-    name: "Shamant Gudigenavar"
-  },
-  {
-    id: 2,
-    name: "Rohit t"
-  },
-  {
-    id: 3,
-    name: "Mahesh P"
-  },
-  {
-    id: 4,
-    name: "Sachin P"
-  },
-  {
-    id: 5,
-    name: "Shridhar K"
-  }
-  ]
+
 
 
 }
